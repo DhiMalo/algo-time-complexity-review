3d9d0201ddd14ad592ab8bed43de1eba872de7aa
Aheri Stanford-Asiyo <aherisan@gmail.com>
2016-02-16 18:00:19 -0800

    Completed through prompt 5 with comments

--- algo-time-complexity-review.js
+++ algo-time-complexity-review.js
@@ -1,22 +1,36 @@
 /////////// Prompt 1 ///////////
-/////////// time complexity: 
 function findMax(array){
-  var max = -Infinity;
-  for (var i = 0; i < array.length; i++){
-    if (array[i] > max){
-      max = array[i];
+  var max = -Infinity; // + 1
+  for (var i = 0; i < array.length; i++){ // for loop : n
+    if (array[i] > max){  // + 1
+      max = array[i]; // + 0 or 1
     }
   }
-  return max; 
+  return max; // this is not an operation per se, but if it were, it would be +1
 }
 
+//My comments: We consider the max number of times that var max will be reset, which is the array length (n). 
+//My time complexity assessment: 1 + n (1 + 1) + 1 = 2n + 2 = O(n) = LINEAR TIME
 
 /////////// Prompt 2 ///////////
-/////////// time complexity: 
 function contains(array, target){
-  return array.indexOf(target) > -1;
+  return array.indexOf(target) > -1; // n
 }
 
+//My comments: The indexOf method in Javascript implements a for-loop to check every value in the input array.
+function indexOf(){ 
+    var index = -1; // + 1
+    for (var i = 0; i < array.length; i++){ // n
+      if (array[i] === target){ // +1 assess
+        return index; // +0 or +1 return
+      }
+    }
+    return index; // +1 at most
+}
+// this internal method has time complexity of 1 + n (1 + 1) + 1 = 2n +2
+
+// My time complexity assessment: Time complexity for Prompt 2 should be 2n +2 PLUS returning the result (+1).
+// which is 2n + 3 ... which again resolves to O(n) = LINEAR TIME
 
 /////////// Prompt 3 ///////////
 /////////// time complexity: 
@@ -24,30 +38,59 @@ function partialContains(array, target, start){
   return array.slice(start).indexOf(target) > -1;
 }
 
+//My comments: We already assessed that the indexOf method in Javascript results in O(n) time complexity. 
+  // This function adds a .slice method. We know that slice makes a [modified] copy of the array.
+  // If the slice method is simply 1 additional step, it should be O(n) + 1 or O(n). However, we must then return a boolean
+  // which is its own operation.  
+  
+function partialContains(array, target, start){
+  var newArray = array.slice(); // + 1
+  var index = newArray.indexOf(target); // n
+  return index > -1; // [+1]
+}
+  
+//My time complexity assessment: n,  which resolves to O(n) = LINEAR TIME
 
 /////////// Prompt 4 ///////////
-/////////// time complexity: 
 function square(array){
-  for (var i = 0; i < 3; i++){
-    array[i] = array[i] * array[i];
+  for (var i = 0; i < 3; i++){ // a for loop that only iterates 0 to 2 =  +3 operations
+    array[i] = array[i] * array[i]; // + 2 operations (multiplication and assignment)
   }
-  return array;
+  return array; // [+1]
 }
+
+//My time complexity assessment: 3(2) + 1 = 6 + 1 = 7 which is 0(c) or CONSTANT TIME 
 
 /////////// Prompt 5 ///////////
-/////////// time complexity: 
 function repeat(array){
-  var repeat = [];
-  for (var j = 0; j < 10; j++){
-    repeat[j] = [];
-    for (var i = 0; i < array.length; i++){
-      repeat[j].push(array[i]);
+  var repeat = []; // +1
+  for (var j = 0; j < 10; j++){ // n
+    repeat[j] = []; // + 10 (create 10 empty arrays)
+    for (var i = 0; i < array.length; i++){ // n
+      repeat[j].push(array[i]); // 1
     }
   }
-  return repeat; 
+  return repeat; // [+1]
 }
-//what if we replace 10 with a parameter? 
 
+// My time complexity assessment: 1 + n ( 10(n (1)) ) + 1 = n (1n x 10) + 2  = n^2 + 10n + 2 = O(n^2) or QUADRATIC TIME
+
+// What if we replace 10 with a parameter? 
+// Hmm, if we did that, we'd have a function that looks like this:
+
+function repeat(array, number){
+  var repeat = []; // +1
+  for (var j = 0; j < number; j++){ // n
+    repeat[j] = []; // + n (create n empty arrays)
+    for (var i = 0; i < array.length; i++){ // n
+      repeat[j].push(array[i]); // 1
+    }
+  }
+  return repeat; // [+1]
+}
+
+// In that case, we would create a "parameter number" of empty arrays, rather than 10.  
+// New time complexity assessment: 1 + n ( n(n (1)) ) + 1 = n(n(n(1))) + 2 = n^3 +2 = O(n^3) or CUBIC TIME!
 
 /////////// Prompt 6 ///////////
 /////////// time complexity: 
